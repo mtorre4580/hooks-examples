f3d5ee0b30f0854bba589d127025bf432e78cfdd
mtorre <daniel.torre@mercadolibre.com>
2019-11-18 20:11:07 -0300

    Updates

---
+++ precache-manifest.b934472fbfb30061ffc0e937621923ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfea4467813d75d03563e7bcaa834654",
+    "url": "/hooks-examples/index.html"
+  },
+  {
+    "revision": "7ccea1e2976e5ba1fa35",
+    "url": "/hooks-examples/static/css/main.c300ebdb.chunk.css"
+  },
+  {
+    "revision": "7c321fa6ba7aa928ce3c",
+    "url": "/hooks-examples/static/js/2.b805e51b.chunk.js"
+  },
+  {
+    "revision": "7ccea1e2976e5ba1fa35",
+    "url": "/hooks-examples/static/js/main.4ddb87de.chunk.js"
+  },
+  {
+    "revision": "f5ee983bdfd195bf4a4d",
+    "url": "/hooks-examples/static/js/runtime-main.8c300a65.js"
+  }
+]);
